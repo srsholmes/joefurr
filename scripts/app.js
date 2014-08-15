1b48ad9023f1cd22999979ba4c33cfeb4c39582f
Simon Holmes <srsholmes@gmail.com>
2014-08-15 12:15:07 +0100

    upadtes to include scrolling animations

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,32 @@
 var ƒ = require('hdom');
-var scran = require('scran');
+var $s = require('scran');
 ƒ(function() {
    
 	console.log('hello my joe');
 
+	$s('.cv-section')
+		.each(function(i, len){
+			var $this = $s(this);
+			$this
+				.force(true)
+				.transform()
+					.start(function(){
+						return 424 + (i * $this.height());
+					})
+					.end(function(){
+						return 424 + ((i + 1) * $this.height());
+					})
+					.opacity({
+						opacity : [0,1]
+					})
+					.translateX({
+						tx : [
+							function() {
+								return (i % 2 === 0) ? $s.windowWidth() * 2 : -($s.windowWidth() * 2);
+							},
+							0
+						]
+					}, $s.easing.easeInOutCirc)
+		});
     
 });
