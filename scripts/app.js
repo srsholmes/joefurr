94ee847a4661849e895e8c8f5cc7f9ecf60d86de
Simon Holmes <srsholmes@gmail.com>
2014-08-18 13:40:14 +0100

    upadtes to include scrolling animations

--- scripts/app.js
+++ scripts/app.js
@@ -4,29 +4,29 @@ var $s = require('scran');
    
 	console.log('hello my joe');
 
-	$s('.cv-section')
-		.each(function(i, len){
-			var $this = $s(this);
-			$this
-				.force(true)
-				.transform()
-					.start(function(){
-						return 424 + (i * $this.height());
-					})
-					.end(function(){
-						return 424 + ((i + 1) * $this.height());
-					})
-					.opacity({
-						opacity : [0,1]
-					})
-					.translateX({
-						tx : [
-							function() {
-								return (i % 2 === 0) ? $s.windowWidth() * 2 : -($s.windowWidth() * 2);
-							},
-							0
-						]
-					}, $s.easing.easeInOutCirc)
-		});
+	// $s('.cv-section')
+	// 	.each(function(i, len){
+	// 		var $this = $s(this);
+	// 		$this
+	// 			.force(true)
+	// 			.transform()
+	// 				.start(function(){
+	// 					return 124 + (i * $this.height());
+	// 				})
+	// 				.end(function(){
+	// 					return 124 + ((i + 1) * $this.height());
+	// 				})
+	// 				.opacity({
+	// 					opacity : [0,1]
+	// 				})
+	// 				.translateX({
+	// 					tx : [
+	// 						function() {
+	// 							return (i % 2 === 0) ? $s.windowWidth() * 2 : -($s.windowWidth() * 2);
+	// 						},
+	// 						0
+	// 					]
+	// 				}, $s.easing.easeInOutCirc)
+	// 	});
     
 });
