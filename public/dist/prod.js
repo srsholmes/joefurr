279b3c899fb257d14cfa80c4de3b6898e4fe020a
Simon Holmes <simon.holmes@holler.co.uk>
2014-10-27 18:12:54 +0000

    slider working

--- public/dist/prod.js
+++ public/dist/prod.js
@@ -2702,14 +2702,9 @@ module.exports = function(ƒ,w,d) {
              */
             var ƒWrapper = ƒ(el),
                 ƒUl = ƒWrapper.find('ul.slider'),
-                ƒNubbins = ƒWrapper.find('.nubbins'),
-                ƒNubbinsUl = ƒNubbins.find('ul'),
-                ƒNubbinsLi = ƒNubbinsUl.find('li'),
+
                 ƒLis = ƒUl.find('li'),
                 ƒfirstClone = ƒLis.get(0).clone(true),
-                // ƒfirstClone2 = ƒLis.get(1).clone(true),
-                // ƒfirstClone3 = ƒLis.get(2).clone(true),
-                // ƒfirstClone4 = ƒLis.get(3).clone(true),
                 ƒlastClone = ƒLis.get(-1).clone(true),
                 liLength = ƒUl.find('li').length,
                 position = ƒWrapper.hasClass('catchup') ? 2 : 1,
@@ -2719,6 +2714,9 @@ module.exports = function(ƒ,w,d) {
 
             var next = ƒWrapper.find('.nav-left');
             var prev = ƒWrapper.find('.nav-right');
+            var currentCounter = ƒWrapper.find('span.current');
+            var totalCounter = ƒWrapper.find('span.total');
+            totalCounter[0].innerText = liLength;
 
             ƒfirstClone.addClass('first-clone');
             ƒlastClone.addClass('last-clone');
@@ -2775,11 +2773,7 @@ module.exports = function(ƒ,w,d) {
                         to = dir === 'next' ? currentValue - elementWidth(ƒWrapper[0]) : currentValue + elementWidth(ƒWrapper[0]);
                         position = dir === 'next' ? position + 1 : position - 1;
 
-                        // if (position < 0 || position > liLength - 1) {
-                        //     canTransition = true;
-                        //     position = oldPos;
-                        //     return;
-                        // }
+
                     } else {
                         /*
                          * dir is a Number from tapping a nubbin
@@ -2787,37 +2781,11 @@ module.exports = function(ƒ,w,d) {
                         position = dir;
                         to = -(position * elementWidth(ƒWrapper[0]));
                     }
-
-                    // if (next && prev) {
-                    //     if (position === (liLength - 1)) {
-                    //         ƒ(next).addClass('hide');
-                    //         ƒ(prev).removeClass('hide');
-                    //     } else if (position === 0){
-                    //         ƒ(prev).addClass('hide');
-                    //         ƒ(next).removeClass('hide');
-                    //     }
-                    // }
-
-
                     /*
                      *  Reset the position properly for infinite looping
                      */
                     var actualPos = position < 1 ? liLength : position > liLength  ? 1 : position;
 
-                    /*
-                     * Reset and select the corrent nubbin
-                     */
-                    // for (var j = 0; j < ƒNubbinsLi.length; j++) {
-                    //     var dNub = ƒNubbinsLi[j];
-                    //     dNub.className = actualPos === (j + 1) ? 'active' : '';
-                    // }
-
-                    /*
-                     * Interpol leverages requestAnimationFrame to create smooth
-                     * interpolation of values. Essentially each step we take the
-                     * value of the interpolation and set the margin left of the list
-                     * to the value.
-                     */
                     Interpol.tween()
                         .from(currentValue)
                         .to(to)
@@ -2827,33 +2795,20 @@ module.exports = function(ƒ,w,d) {
                             ƒUl[0].style[CSS_TRANSFORM] = "translateX(" + val + "px)";
                         })
                         .complete(function() {
-                            //Needs some logic to detect when it is the end of the slide
-
                             canTransition = true;
                             position = actualPos;
-                            render();
-
-                        
+                            render();                        
                         })
                         .start();
+
+
+                        currentCounter[0].innerText = actualPos;
                 };
             }
-
-            // for (var j = 1; j < liLength; j++) {
-            //     ƒNubbinsUl[0].appendChild(ƒNubbinsLi[0].cloneNode(true));
-            // }
-            // ƒNubbinsLi = ƒNubbinsUl.find('li');
-            // ƒNubbinsLi[position - 1].className = 'active';
-            // for (var k = 0; k < ƒNubbinsLi.length; k++) {
-            //     var dNub = ƒNubbinsLi[k];
-            //     dNub.addEventListener('tap', move(k + 1));
-            //     dNub.addEventListener('click', move(k + 1));
-            // }
 
             /*
              * Add listeners for our custom events.
              */
-
 
             ƒWrapper.bind('swipeleft', move('next'));
             ƒWrapper.bind('swiperight', move('right'));
@@ -2867,17 +2822,10 @@ module.exports = function(ƒ,w,d) {
 
             ƒLis[0].parentNode.insertBefore(ƒlastClone[0], ƒLis[0]);
             ƒUl[0].appendChild(ƒfirstClone[0]);
-            // ƒUl[0].appendChild(ƒfirstClone2[0]);
-            // ƒUl[0].appendChild(ƒfirstClone3[0]);
-            // ƒUl[0].appendChild(ƒfirstClone4[0]);
+
 
             render();
 
-
-            /*
-             * Listen to the window resize event to determine if the
-             * width of the wrapper has changed.
-             */
             window.addEventListener('resize', render);
         });
 
