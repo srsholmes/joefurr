56b8df45c9824ca87aa914ae6f27e39c06261a1a
Simon Holmes <simon.holmes@holler.co.uk>
2014-10-27 18:32:43 +0000

    contact me scroll

--- public/dist/prod.js
+++ public/dist/prod.js
@@ -2666,13 +2666,26 @@ var Interpol = require('interpol');
 
 module.exports = function() {
     function init() {
-        console.log('scroll top');
+
         ƒ('.go-to-top').bind('click', function(event) {
             var top = (window.pageYOffset || document.scrollTop)  - (document.clientTop || 0);
             console.log(top);
             Interpol.tween()
                 .from(top)
                 .to(0)
+                .ease(Interpol.easing.easeInOutCirc)
+                .step(function(val) {
+                    window.scrollTo(0, val)
+                })                
+                .start();
+        });
+
+        ƒ('a.nav-Contact').bind('click', function(event) {
+            event.preventDefault();
+            var pageH = ƒ('body').height();
+            Interpol.tween()
+                .from(0)
+                .to(pageH)
                 .ease(Interpol.easing.easeInOutCirc)
                 .step(function(val) {
                     window.scrollTo(0, val)
